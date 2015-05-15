930476d3c167f693601845e663244506f1c9c09b
xiaogeng <xiaogeng91@gmail.com>
2015-05-15 21:30:55 +0800

    merge

--- src/network/APNetwork.java
+++ src/network/APNetwork.java
@@ -167,14 +167,7 @@ public enum APNetwork implements OverlayNetwork {
 		
 		try {
 			if(connectedFalg){
-				outputStream = new ObjectOutputStream(socket.getOutputStream());
-				outputStream.writeObject(msg);
-				outputStream.flush();
-				if(countSend < 10){
-					Log.i(TAG, "send msg "+msg.toString());
-					countSend++;
-				}
-				/*
+				
 				outputStream.reset();
 				outputStream.writeObject(msg);
 				outputStream.flush();
@@ -182,7 +175,6 @@ public enum APNetwork implements OverlayNetwork {
 					Log.i(TAG, "send msg "+msg.toString());
 					countSend++;
 				}
-				*/
 				//
 			}
 		} catch (IOException e) {
