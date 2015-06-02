ef8d9650d36e463cda98c3070c986599f9014029
xiaogeng <xiaogeng91@gmail.com>
2015-06-02 15:54:53 +0800

    merge

--- src/buffer/ReceiveQueueThread.java
+++ src/buffer/ReceiveQueueThread.java
@@ -1,20 +1,11 @@
 package buffer;
 
-import game.GameModel;
 import game.sharedmemory.communication.MessagingService;
 import game.sharedmemory.communication.message.IPMessage;
-import game.sharedmemory.data.Key;
-import game.sharedmemory.data.Value;
-import game.sharedmemory.data.Version;
-import game.sharedmemory.data.VersionValue;
-
 import java.util.concurrent.BlockingQueue;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-
-import constant.Constant;
 /**
  * @author XiaoGeng
  * 该类专门用来处理receiveQueue队列中的数据，当队列为空时，阻塞。
