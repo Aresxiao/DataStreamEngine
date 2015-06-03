53ffd5bbc1b93f834bf53dd790ebb352663fda83
xiaogeng <xiaogeng91@gmail.com>
2015-06-03 21:26:21 +0800

    get a versionvalue can only get its clone

--- src/buffer/ReceiveQueueThread.java
+++ src/buffer/ReceiveQueueThread.java
@@ -6,12 +6,16 @@ import java.util.concurrent.BlockingQueue;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.util.Log;
 /**
  * @author XiaoGeng
  * 该类专门用来处理receiveQueue队列中的数据，当队列为空时，阻塞。
  */
 
 public class ReceiveQueueThread extends Thread{
+	
+	private static String TAG = ReceiveQueueThread.class.getName();
 	
 	int receiveCount = 0;
 	BlockingQueue<String> receiveQueue;
@@ -29,6 +33,9 @@ public class ReceiveQueueThread extends Thread{
 				
 				JSONObject jsonObject = new JSONObject(data);
 				IPMessage msg = IPMessage.getIPMessageFromJSON(jsonObject);
+				if(msg == null){
+					Log.i(TAG, "get a msg is null");
+				}
 				MessagingService.INSTANCE.onReceive(msg);
 				
 			} catch (InterruptedException e) {
